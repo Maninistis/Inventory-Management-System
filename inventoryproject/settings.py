ddedec79dde90440f6443b8879ddc1f589de9c3a
Maninistis <arizjoshuaalberto@gmail.com>
2025-08-12 21:28:51 +0800

    to edit colors maybe add css

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -135,6 +135,16 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'djangoinventorydemo@gmail.com'
+EMAIL_HOST_PASSWORD = 'gyqg tgjf cvzd nzrv'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
